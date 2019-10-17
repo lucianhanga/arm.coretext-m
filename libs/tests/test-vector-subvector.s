9a08922e379e0be3a0995a18ddb933b3a43bafce
BuildTools <unconfigured@null.spigotmc.org>
2019-10-17 16:18:35 +0200

    fix the function error

--- libs/tests/test-vector-subvector.s
+++ libs/tests/test-vector-subvector.s
@@ -11,6 +11,14 @@ sarr1:      .word   10, 11, 12
             .equ    sarr1_size, (. - sarr1) >> 2
 sarr2:      .word   10, 11, 13
             .equ    sarr2_size, (. - sarr2) >> 2
+sarr3:      .word   28, 29, 30
+            .equ    sarr3_size, (. - sarr3) >> 2
+sarr4:      .word   10, 11, 12, 13 ,14, 15, 16, 17, 18, 19, 20
+            .word   21, 22, 23, 24, 25, 26, 27, 28, 29, 30
+            .equ    sarr4_size, (. - sarr4) >> 2
+sarr5:      .word   28, 29, 31
+            .equ    sarr5_size, (. - sarr5) >> 2
+
 fmt_txt:    .string "array:    "
 fmt2_txt:   .string "subarray: "
 fmt_nr:     .string "%02d "
@@ -32,6 +40,84 @@ main:       stmfd   sp!, {lr}
             ldr     r2, =sarr1
             mov     r3, #sarr1_size
             bl      lh_print_array
+            @ do the search of sub-vector in vector
+            ldr     r0, =arr
+            mov     r1, #arr_size
+            ldr     r2, =sarr1
+            mov     r3, #sarr1_size
+            bl      lh_vector_subvector
+            @ print the comparation result
+            mov     r1, r0
+            ldr     r0, =fmt_ret
+            bl      printf
+
+            @ print the sub array 2
+            ldr     r0, =fmt2_txt
+            ldr     r1, =fmt_nr
+            ldr     r2, =sarr2
+            mov     r3, #sarr2_size
+            bl      lh_print_array
+            @ do the search of sub-vector in vector
+            ldr     r0, =arr
+            mov     r1, #arr_size
+            ldr     r2, =sarr2
+            mov     r3, #sarr2_size
+            bl      lh_vector_subvector
+            @ print the comparation result
+            mov     r1, r0
+            ldr     r0, =fmt_ret
+            bl      printf
+
+            @ print the sub array 3
+            ldr     r0, =fmt2_txt
+            ldr     r1, =fmt_nr
+            ldr     r2, =sarr3
+            mov     r3, #sarr3_size
+            bl      lh_print_array
+            @ do the search of sub-vector in vector
+            ldr     r0, =arr
+            mov     r1, #arr_size
+            ldr     r2, =sarr3
+            mov     r3, #sarr3_size
+            bl      lh_vector_subvector
+            @ print the comparation result
+            mov     r1, r0
+            ldr     r0, =fmt_ret
+            bl      printf
+
+            @ print the sub array 4
+            ldr     r0, =fmt2_txt
+            ldr     r1, =fmt_nr
+            ldr     r2, =sarr4
+            mov     r3, #sarr4_size
+            bl      lh_print_array
+            @ do the search of sub-vector in vector
+            ldr     r0, =arr
+            mov     r1, #arr_size
+            ldr     r2, =sarr4
+            mov     r3, #sarr4_size
+            bl      lh_vector_subvector
+            @ print the comparation result
+            mov     r1, r0
+            ldr     r0, =fmt_ret
+            bl      printf
+
+            @ print the sub array 5
+            ldr     r0, =fmt2_txt
+            ldr     r1, =fmt_nr
+            ldr     r2, =sarr5
+            mov     r3, #sarr5_size
+            bl      lh_print_array
+            @ do the search of sub-vector in vector
+            ldr     r0, =arr
+            mov     r1, #arr_size
+            ldr     r2, =sarr5
+            mov     r3, #sarr5_size
+            bl      lh_vector_subvector
+            @ print the comparation result
+            mov     r1, r0
+            ldr     r0, =fmt_ret
+            bl      printf
 
 ret:        mov     r0, #0
             ldmfd   sp!, {pc}
--- libs/vector/subvector.s
+++ libs/vector/subvector.s
@@ -14,19 +14,54 @@
 @   r1: size of the vector
 @   r2: address of the subvector (pointer to an unsigned 32bit value)
 @   r3: size of the subvector
+@
+@   ret:  -1   the substring was not found 
+@         >=0  the substring was found and the return value is the index
+@
 lh_vector_subvector:
             stmfd   sp!, {lr}       @ store the Link Register
-            stmfd   sp!, {r4-r7}    @ store the non-volatile registers
-
+            stmfd   sp!, {r4-r10}   @ store the non-volatile registers
             @ function parameters
 p_arr       .req    r0
 arr_size    .req    r1
 p_sarr      .req    r2
 sarr_size   .req    r3
             @ local variables
+i_end       .req    r4
+i           .req    r5
+j           .req    r6
+val1        .req    r7
+val2        .req    r8
+rcode       .req    r9
+i_j         .req    r10
 
 
+            cmp     sarr_size, arr_size @ if the subvec. bigger than the vec.
+            movgt   r0, #-1             @ return -1
+            bgt     ret                 @ exit function
 
-ret:        ldmfd   sp!, {r4-r7}    @ restore the nonvolatile registers
+            sub     i_end, arr_size, sarr_size
+            mov     i, #0           @ beginning of the outer array
+i_loop:     cmp     i, i_end        @ check if the outer array is finished
+            movgt   r0, #-1         @ if it exits from here ret: -1; no match
+            bgt     ret             @ return from the function.
+            mov     j, #0           @ beginning of the inner array
+j_loop:     cmp     j, sarr_size    @ check the exit condition for inner loop
+            movge   r0, i           @ if exit from here the match was found: i
+            bge     ret             @ exit the inner loop and function
+            add     i_j, i, j       @ calculate the index for out vec
+            ldr     val1, [p_arr, i_j, lsl #2] @ load the value from out vec
+            ldr     val2, [p_sarr, j, lsl #2]  @ load the value from innen
+            cmp     val1, val2      @ compare the values
+            movne   rcode, #-1      @ if not-equal rcode  <- -1
+            bne     j_loop_exit     @ if not-equal exit the inner loop
+            add     j, j, #1        @ increase the j index
+            b       j_loop          @ close the inner loop
+j_loop_exit:
+            add     i, i, #1        @ increaze the i index
+            b       i_loop          @ close the outer loop
+i_loop_exit:
+
+ret:        ldmfd   sp!, {r4-r10}   @ restore the nonvolatile registers
             ldmfd   sp!, {lr}       @ restore the Link Register
             mov     pc, lr          @ return to the caller
